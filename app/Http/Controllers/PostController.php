066885ff349d0482893fd3340cf3c3b219917f81
kakudaisuke <dk.15step@gmail.com>
2020-09-04 01:16:51 +0900

    投稿画面用View,Controller,Routing準備OK（ブラウザでも表示できた）

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index() {
+        $data = [
+            'user'=>'ゲスト',
+        ];
+        return view('post', $data); //folder.file
+    }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,9 +4,24 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+  <link rel="stylesheet" href="styles.css">
   <title>Jazz Log</title>
+  <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
-  
+  <form action="" id="create-account">
+      <h1>{{$user}}さん、ライブの感想をどうぞ</h1>
+      <label for="live_date">ライブに行った日</label>
+      <input type="date" id="live_date">
+      <label for="musician">ミュージシャン</label>
+      <input type="text" id="musician">
+      <label for="venue">ライブ会場</label>
+      <input type="text" id="venue">
+      <label for="review">ライブの感想</label>
+      <textarea name="" id="review" cols="30" rows="10"></textarea>
+      <p>1000文字以内</p>
+      <button>投稿する</button>
+    </form>
 </body>
 </html>
