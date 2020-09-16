b97e73d32a0492cf688565d95f51b82abf23d8a8
kakudaisuke <dk.15step@gmail.com>
2020-09-16 15:28:22 +0900

    ReviewControllerにpostメソッド、createメソッド追加

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -6,10 +6,26 @@ use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    //
+    //トップページを表示
     public function index(Request $request)
     {
         $items = Review::all();
         return view('index', ['items' => $items]);
     }
+    //投稿ページを表示
+    public function post(Request $request)
+    {
+        return('post');
+    }
+    //フォームの値を取得しDBにレコード挿入
+    public function create(Request $request)
+    {
+        $this->validate($request, Review::$rules); //バリデーションの実行
+        $board = new Review; //Reviewインスタンス作成
+        $form = $request->all(); //送信されたフォームの値を保管
+        unset($form['_token']); //CSRF非表示フィールド_token削除
+        $board->fill($form)->save(); //fillメソッドでモデルのプロパティにまとめて代入
+        return redirect('/'); //トップページへ
+    }
+
 }
