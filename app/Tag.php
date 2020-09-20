77d61107a285e87113d9bb830506806a514c2023
kakudaisuke <dk.15step@gmail.com>
2020-09-20 16:08:43 +0900

    全角スペースを許さないバリデーション検討中

--- app/Tag.php
+++ app/Tag.php
@@ -8,9 +8,9 @@ class Tag extends Model
 {
     protected $guarded = array('id');
 
-    public static $rules = array(
-        'tag' => 'tag_name',
-    );
+    //public static $rules = array(
+    //    'tag_name' => 'not_in: 　', //全角スペースを入らないようにしたい
+    //);
 
     //
     public function reviews()
