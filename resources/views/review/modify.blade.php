8e1e2fdf5ebbf9ba72d98eec9c4beff3dc33d441
kakudaisuke <dk.15step@gmail.com>
2020-09-27 01:59:44 +0900

    投稿修正ページを継承レイアウトで再構築

--- resources/views/review/modify.blade.php
+++ resources/views/review/modify.blade.php
@@ -1,59 +1,24 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.ourapp')
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-  <title>Jazz Log</title>
-  <style>
-    html, body {
-      background-color: #f5F5F5;
-      color: #636b6f;
-      font-family: 'Nunito', sans-serif;
-      font-weight: 200;
-      height: 100vh;
-      margin: 0;
-    }
-    .form_container {
-      width: 70%;
-      height: 800px;
-      margin: 100px auto;
-      background-color: #fff;
-    }
-    form {
-      width: 80%;
-      margin: 2px auto;
-      padding-top: 40px;
-      display: flex;
-      flex-direction: column;
-    }
-    label, input, textarea {
-      padding: 3px 0;
-    }
-    input, textarea {
-      border: .5px solid rgb(219, 217, 217);
-      padding: 5px 8px;
-    }
-    .submit {
-      width: 240px;
-      font-size: 15px;
-      text-align: center;
-      font-weight: bold;
-      border: none;
-      border-radius: 5px;
-      background-color: rgb(252, 180, 48);
-      color: rgb(39, 37, 37);
-      margin: 15px auto;
-      padding: 8px;
-    }
-    #del {
-      background-color: #DCDCDC;
-    }
-  </style>
-</head>
-<body>
-  <div class="form_container">
+@section('title', '投稿を修正する')
+
+@section('css')
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css Heroku-->
+  <link type="text/css" rel="stylesheet" href="{{ secure_asset('css/ourmaterialize.css') }}"  media="screen,projection"/>
+  <!--Import materialize.css Local-->
+  <link type="text/css" rel="stylesheet" href="{{ asset('css/ourmaterialize.css') }}"  media="screen,projection"/>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <!-- Heroku -->
+  <link rel="stylesheet" href="{{secure_asset('css/post.css')}}">
+  <!-- Local -->
+  <link rel="stylesheet" href="{{asset('css/post.css')}}">
+@endsection
+
+@section('main')
+<h1 class="main-title">レビューを修正する</h1>
+<div class="form_container">
     <form action="update" id="create-account" method="POST">
     @csrf
         <h1>投稿を修正する</h1>
@@ -67,7 +32,7 @@
         <input type="text" id="title" name="title" value="{{$form->title}}">
         <label for="text">ライブの感想</label>
         <textarea name="text" id="text" cols="30" rows="10">{{$form->text}}</textarea>
-        <p style="text-align: right; font-size: 0.8rem;">（1000文字以内）</p>
+        <p style="text-align: right; font-size: 0.8rem;">（XXXX文字以内）</p>
         <input type="submit" class="submit" value="修正">
     </form>
     <form action="del" method="post">
@@ -80,5 +45,93 @@
         <input type="submit" class="submit" id="del" value="削除">
     </form>
   </div><!-- /.form_container -->
-</body>
-</html>
+@endsection
+
+@section('js')
+<script type="text/javascript">
+    //今日の日時を表示
+        window.onload = function () {
+            //今日の日時を表示
+            var date = new Date()
+            var year = date.getFullYear()
+            var month = date.getMonth() + 1
+            var day = date.getDate()
+          
+            var toTwoDigits = function (num, digit) {
+              num += ''
+              if (num.length < digit) {
+                num = '0' + num
+              }
+              return num
+            }
+            
+            var yyyy = toTwoDigits(year, 4)
+            var mm = toTwoDigits(month, 2)
+            var dd = toTwoDigits(day, 2)
+            var ymd = yyyy + "-" + mm + "-" + dd;
+            
+            document.getElementById("live_date").value = ymd;
+        }
+</script>
+<script>//チップスのjQuery
+$(function (){
+  $('.chips').chips();
+  $('.chips-initial').chips({
+    data: [{
+      tag: 'Jazz',
+    }, {
+      tag: '東京',
+    }],
+  });
+  $('.chips-placeholder').chips({
+    placeholder: 'Enter a tag',
+    secondaryPlaceholder: '+Tag',
+  });
+  // $('.chips-autocomplete').chips({
+  //   autocompleteOptions: {
+  //     data: {
+  //       'ブルーノート東京': null,
+  //       'コットンクラブ東京': null,
+  //       "Kelly's大阪": null
+  //     },
+  //    limit: Infinity,
+  //    minLength: 1
+  //  }
+  //});
+});
+</script>
+<!--JavaScript at end of body for optimized loading-->
+<script type="text/javascript" src="{{ asset('js/materialize.js') }}">
+  // document.addEventListener('DOMContentLoaded', function() {
+  //   var elems = document.querySelectorAll('.chips');
+  //   var instances = M.Chips.init(elems, options);
+  // });
+  var instance = M.Chips.getInstance(elem);
+
+  instance.addChip({
+    tag: 'chip content',
+    //image: '', // optional
+  });
+  
+  instance.deleteChip(3); // Delete 3rd chip.
+
+  instance.selectChip(2); // Select 2nd chip
+</script>
+<script>
+  let tag = document.querySelector('.chip'); //.chipクラスの要素を取得
+  let tagName = tag.innerHTML;//タグ内の要素を取得→エラー
+  //let tagName = "france";//タグ内の要素を取得
+  let inputTag = document.getElementById("tag");//<input id="tag">を取得
+  let submit = document.querySelector('.submit');//送信ボタンを取得
+
+  function tagSubmit() {
+    inputTag.innerHTML = tagName;//tagNameを<input id="tag">の値として追加
+    //inputTag.textContent = 'france';//tagNameを<input id="tag">の値として追加
+  }
+
+  submit.addEventListener('submit', () => {
+    tagSubmit();
+  });
+  // innerHTMLプロパティ
+</script>
+@endsection
