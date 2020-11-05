15728781553335c805b7a312700a0dfe9b2dcd9d
ayakaaa182 <ayakaaa.182@gmail.com>
2020-11-05 22:22:37 +0900

    CSS RESET設定しました

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS RESET -->
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -14,15 +16,15 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- font awesome -->
     <script src="https://kit.fontawesome.com/aaab412f99.js" crossorigin="anonymous"></script>
-    
+
     @env('local')
-        <link rel="stylesheet" href="{{asset('css/styles.css')}}">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}" defer></script>
     @endenv
     @production
-        <link rel="stylesheet" href="{{secure_asset('css/styles.css')}}">
-        <script src="{{secure_asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{secure_asset('css/styles.css')}}">
+    <script src="{{secure_asset('js/app.js') }}" defer></script>
     @endproduction
     @yield('css')
 </head>
@@ -30,13 +32,14 @@
 <body>
     <div id="app">
         @component('components.header')
-            @slot('user')
-                @if(Auth::check())<!--ログインしているかの確認 -->
-                     {{Auth::user()->account_name}}
-                @else
-                    ゲスト
-                @endif
-             @endslot
+        @slot('user')
+        @if(Auth::check())
+        <!--ログインしているかの確認 -->
+        {{Auth::user()->account_name}}
+        @else
+        ゲスト
+        @endif
+        @endslot
         @endcomponent
         <main class="py-4">
             @yield('content')
