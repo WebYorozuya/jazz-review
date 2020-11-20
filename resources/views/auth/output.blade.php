d3a6561d3373cab1932fe2ee30f56d440fa256fd
kakudaisuke <dk.15step@gmail.com>
2020-11-20 22:23:57 +0900

    brタグはやめよう

--- resources/views/auth/output.blade.php
+++ resources/views/auth/output.blade.php
@@ -22,15 +22,13 @@
       @endforeach
     </ul>
   @endif
-  <h1>プロフィール画像を設定する</h1>
-  <form method="post" action="{{ route('upload_image') }}" enctype="multipart/form-data">
-    @csrf
-    <input type="file" name="image" accept="image/png, image/jpeg">
-    <br>
-    <input type="submit" value="Upload">
-  </form>
+    <h1>プロフィール画像を設定する</h1>
+    <form method="post" action="{{ route('upload_image') }}" enctype="multipart/form-data">
+      @csrf
+      <input type="file" name="image" accept="image/png, image/jpeg">
+      <input type="submit" value="Upload">
+    </form>
   @foreach($user_images as $user_image)
     <img class='image-round1' src="{{ Auth::user()->user_image }}">
-    <br>
   @endforeach
 @endsection
